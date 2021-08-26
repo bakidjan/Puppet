bc739c5a98a4b70c39f735c1443950ac715fe263
Bakidjan <55193201+bakidjan@users.noreply.github.com>
2021-08-26 19:03:16 +0200

    Update nginx-v1.pp

--- nginx-v1.pp
+++ nginx-v1.pp
@@ -1,18 +1,14 @@
 #Diallo 
 package { 'nginx':
-
   ensure => 'present',
 }
 
 service { 'nginx':
-
   ensure => 'running',
-
   enable => 'true',
 }
 
 file { '/usr/share/nginx/html/index.html':
-
   content => "Welcome to nginx-v1.pp By DIALLO on puppet server",
 }
 
